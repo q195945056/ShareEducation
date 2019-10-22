565ee4ee4b1582a5662526d9129e91754c9a78be
严明俊 <yanmingjun@youloft.com>
2019-10-22 17:05:20 +0800

    Initial Commit

---
+++ ShareEducation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShareEducation
+//
+//  Created by 严明俊 on 2019/10/22.
+//  Copyright © 2019 严明俊. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
