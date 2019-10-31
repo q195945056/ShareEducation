6ad70c307d0402b10a1850066bfb0032e138ac8a
YanMingJun <195945056@qq.com>
2019-10-31 17:56:31 +0800

    搭框架

---
+++ Podfile
@@ -0,0 +1,17 @@
+# Uncomment the next line to define a global platform for your project
+ platform :ios, '10.0'
+
+target 'ShareEducation' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for ShareEducation
+  
+  pod 'SnapKit'#Autolayout
+  pod 'Alamofire'#网络请求
+  pod 'Kingfisher'#加载网络图片
+  pod 'SwiftyJSON'#json串解析
+  pod 'LLCycleScrollView'#循环滚动
+  pod 'Segmentio'#SegmentedControl
+
+end
---
+++ ShareEducation/Classes/Mine/Controller/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  ShareEducation
+//
+//  Created by 严明俊 on 2019/10/23.
+//  Copyright © 2019 严明俊. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
